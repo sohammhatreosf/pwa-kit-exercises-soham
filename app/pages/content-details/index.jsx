32a270bfeb6ad664c5ff328230f43369f92bdf71
sohammhatreosf <soham.mhatre@osf.digital>
2022-10-04 12:56:06 +0530

    Update index.jsx

--- app/pages/content-details/index.jsx
+++ app/pages/content-details/index.jsx
@@ -2,24 +2,29 @@ import React from 'react'
 import fetch from 'cross-fetch'
 import { HTTPError } from 'pwa-kit-react-sdk/ssr/universal/errors'
 
-const ContentDetails = ({ contentResult }) => {
+const ContentDetails = ({ contentResult, error }) => {
+    if (error) {
+        return <div>{error.fault.message}</div>
+    }
     if (!contentResult) {
         return <div>Loading...</div>
     }
     return <div dangerouslySetInnerHTML={{ __html: contentResult.c_body }} />
 }
-ContentDetails.getProps = async ({ params }) => {
-    let contentResult
+ContentDetails.getProps = async ({ params, res }) => {
+    let contentResult, error
     const result = await fetch(
         `http://localhost:3000/mobify/proxy/ocapi/s/RefArch/dw/shop/v20_2/content/${params.id}?client_id=d53e494e-20f4-4c40-9c22-b6f5146709aa`
-   )
-   if (result.ok) {
-   contentResult = await result.json()
-   } else {
-   const error = await result.json()
-   throw new HTTPError(result.status, error.fault.message)
-   }
-   return {contentResult}
+    )
+    if (result.ok) {
+        contentResult = await result.json()
+    } else {
+        error = await result.json()
+        if (res) {
+            res.status(result.status)
+        }
+    }
+    return { contentResult, error }
 }
 ContentDetails.getTemplateName = () => 'content-details'
 export default ContentDetails
